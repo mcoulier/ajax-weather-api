8e0edc103f78bacc4e24c388e10f5f80a169bfdf
Michiel Coulier <michielcoulier@gmail.com>
2020-09-16 14:21:16 +0200

    used correct api called forecast

--- script.js
+++ script.js
@@ -4,11 +4,12 @@
 document.getElementById("search").addEventListener("click", getWeather)
 
         function getWeather() {
-            let city = document.getElementById("city").value
-            axios.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + mykey)
+
+            let city = document.getElementById("city").value;
+
+            axios.get('http://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + mykey)
                 .then(function (response) {
                     // handle success
-                    console.log(city)
                     console.log(response);
                 })
                 .catch(function (error) {
