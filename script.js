15fbbcba1bd4a924d4601250d32e1a318c2b0e27
Michiel Coulier <michielcoulier@gmail.com>
2020-09-16 12:27:06 +0200

    Added https to link

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.getElementById("search").addEventListener("click", getWeather)
         function getWeather() {
             let city = document.getElementById("city")
             const axios = require('axios');
-            axios.get('api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + mykey)
+            axios.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + mykey)
                 .then(function (response) {
                     // handle success
                     console.log(response);
