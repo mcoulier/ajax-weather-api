ffe31288dd60055c0ba6acfa297359268264a539
Michiel Coulier <michielcoulier@gmail.com>
2020-09-16 14:35:43 +0200

    got temp in console

--- script.js
+++ script.js
@@ -6,11 +6,13 @@ document.getElementById("search").addEventListener("click", getWeather)
         function getWeather() {
 
             let city = document.getElementById("city").value;
+            let weather;
 
             axios.get('http://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + mykey)
-                .then(function (response) {
-                    // handle success
+                .then(response => {
+                    weather = response.data.list[1].main.temp
                     console.log(response);
+                    console.log(weather);
                 })
                 .catch(function (error) {
                     // handle error
