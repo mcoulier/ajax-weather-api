16f1c61a9822012c6ae925a9111f95425cabbf6b
Michiel Coulier <michielcoulier@gmail.com>
2020-09-16 12:38:07 +0200

    forgot to ask value of city id

--- script.js
+++ script.js
@@ -2,13 +2,13 @@
     var mykey = config.MY_KEY;
 
 document.getElementById("search").addEventListener("click", getWeather)
-    {
+
         function getWeather() {
-            let city = document.getElementById("city")
-            const axios = require('axios');
+            let city = document.getElementById("city").value
             axios.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + mykey)
                 .then(function (response) {
                     // handle success
+                    console.log(city)
                     console.log(response);
                 })
                 .catch(function (error) {
@@ -18,5 +18,5 @@ document.getElementById("search").addEventListener("click", getWeather)
 
 
         }
-}
-})()
+
+})();
