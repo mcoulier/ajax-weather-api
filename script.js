afe9a31df9cb915e1961fdd6854e6e11bb44c36f
Michiel Coulier <michielcoulier@gmail.com>
2020-09-17 15:22:03 +0200

    trying to add city name from array

--- script.js
+++ script.js
@@ -11,8 +11,7 @@ document.getElementById("search").addEventListener("click", getWeather)
             let weatherTemp3;
             let weatherTemp4;
             let weatherTemp5;
-            let minTemp;
-            let maxTemp;
+            let cityName;
 
             axios.get('http://api.openweathermap.org/data/2.5/forecast?q=' + city + '&units=metric&appid=' + mykey)
                 .then(response => {
@@ -22,7 +21,7 @@ document.getElementById("search").addEventListener("click", getWeather)
                     weatherTemp3 = response.data.list[3].main.temp
                     weatherTemp4 = response.data.list[4].main.temp
                     weatherTemp5 = response.data.list[5].main.temp
-
+                    cityName = response.data.city[2]
 
                     document.getElementById("temp1").innerHTML = Math.round(weatherTemp1)
                     document.getElementById("temp2").innerHTML = Math.round(weatherTemp2)
@@ -30,15 +29,12 @@ document.getElementById("search").addEventListener("click", getWeather)
                     document.getElementById("temp4").innerHTML = Math.round(weatherTemp4)
                     document.getElementById("temp5").innerHTML = Math.round(weatherTemp5)
 
-
                     console.log(response);
+                    console.log(cityName);
                 })
                 .catch(function (error) {
                     // handle error
                     console.log(error);
                 })
-
-
         }
-
 })();
