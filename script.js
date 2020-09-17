b904591ebd41ee9cba04fe9164f0a598768cb5dd
Michiel Coulier <michielcoulier@gmail.com>
2020-09-17 15:24:19 +0200

    cityname working in html

--- script.js
+++ script.js
@@ -21,13 +21,14 @@ document.getElementById("search").addEventListener("click", getWeather)
                     weatherTemp3 = response.data.list[3].main.temp
                     weatherTemp4 = response.data.list[4].main.temp
                     weatherTemp5 = response.data.list[5].main.temp
-                    cityName = response.data.city[2]
+                    cityName = response.data.city.name
 
                     document.getElementById("temp1").innerHTML = Math.round(weatherTemp1)
                     document.getElementById("temp2").innerHTML = Math.round(weatherTemp2)
                     document.getElementById("temp3").innerHTML = Math.round(weatherTemp3)
                     document.getElementById("temp4").innerHTML = Math.round(weatherTemp4)
                     document.getElementById("temp5").innerHTML = Math.round(weatherTemp5)
+                    document.getElementById("cityName").innerHTML = cityName
 
                     console.log(response);
                     console.log(cityName);
