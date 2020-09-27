1948ad4cf82b43bf11091f5804df5cfc12f361c2
Michiel Coulier <michielcoulier@gmail.com>
2020-09-27 16:50:43 +0200

    main temp loop working

--- script.js
+++ script.js
@@ -12,11 +12,6 @@ document.getElementById("search").addEventListener("click", getWeather)
         function getWeather() {
 
             let city = document.getElementById("city").value;
-            let weatherTemp1;
-            let weatherTemp2;
-            let weatherTemp3;
-            let weatherTemp4;
-            let weatherTemp5;
             let cityName;
             let weatherDesc1;
             let weatherDesc2;
@@ -29,15 +24,11 @@ document.getElementById("search").addEventListener("click", getWeather)
                 .then(response => {
 
                     for (x = 1; x < 6; x++){
-                        weatherTemp = response.data.list[x].main.temp
-                        console.log(x)
+                        let weatherTemp = response.data.list[x].main.temp
+                        document.getElementById("temp"+x).innerHTML = Math.round(weatherTemp) + "°C"
                         console.log(weatherTemp)
                     }
 
-                    weatherTemp2 = response.data.list[2].main.temp
-                    weatherTemp3 = response.data.list[3].main.temp
-                    weatherTemp4 = response.data.list[4].main.temp
-                    weatherTemp5 = response.data.list[5].main.temp
                     cityName = response.data.city.name
                     weatherDesc1 = response.data.list[1].weather[0].description
                     weatherDesc2 = response.data.list[2].weather[0].description
@@ -45,11 +36,6 @@ document.getElementById("search").addEventListener("click", getWeather)
                     weatherDesc4 = response.data.list[4].weather[0].description
                     weatherDesc5 = response.data.list[5].weather[0].description
 
-                    document.getElementById("temp1").innerHTML = Math.round(weatherTemp1) + "°C"
-                    document.getElementById("temp2").innerHTML = Math.round(weatherTemp2) + "°C"
-                    document.getElementById("temp3").innerHTML = Math.round(weatherTemp3) + "°C"
-                    document.getElementById("temp4").innerHTML = Math.round(weatherTemp4) + "°C"
-                    document.getElementById("temp5").innerHTML = Math.round(weatherTemp5) + "°C"
                     document.getElementById("cityName").innerHTML = cityName
                     document.getElementById("desc1").innerHTML = weatherDesc1
                     document.getElementById("desc2").innerHTML = weatherDesc2
